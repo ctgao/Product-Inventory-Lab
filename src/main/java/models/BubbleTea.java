cc42196b74db653618a01b216e4e185b16a608e4
Christine <christine@zipcodersMBP13.localdomain>
2023-08-01 11:34:06 -0400

    added fields for bubbletea

--- src/main/java/models/BubbleTea.java
+++ src/main/java/models/BubbleTea.java
@@ -1,4 +1,12 @@
 package models;
 
 public class BubbleTea {
+    // my variables here
+    private String tea_base;
+    private float iceLevel;
+    private int sugarLevel;
+    private boolean hasBubbles;
+    private boolean hasMilk;
+    private int quantity;
+    private float price;
 }
