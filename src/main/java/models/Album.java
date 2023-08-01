844be830c5efc4428cfea53a5b1f528fa120a85b
Christine <christine@zipcodersMBP13.localdomain>
2023-08-01 11:29:17 -0400

    added fields for album

--- src/main/java/models/Album.java
+++ src/main/java/models/Album.java
@@ -1,4 +1,12 @@
 package models;
 
 public class Album {
+    // my variables here
+    private int id;
+    private String album_name;
+    private String artist_name;
+    private String[] track_listing;
+    private boolean hasPhotoCards;
+    private int quantity;
+    private float price;
 }
